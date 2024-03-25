723a0a80772bfb940a2ea6f7abda879af2b415c3
Ahmed Sufiyan <142326931+IAMAMICROBOT@users.noreply.github.com>
2024-03-25 14:03:54 +0530

    C

---
+++ 0141. Linked List Cycle - Easy.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+    struct ListNode *slow,*fast;
+    slow=fast=head;
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            return true;
+        }
+
+    }
+    return false;
+}
